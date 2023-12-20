dd22e273a18981459fbc5781cfe19c8153113a85
Kumar Chaudhary <kumarbhetwal25@gmail.com>
2023-12-20 20:58:59 +0545

    post blog

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,19 +1,70 @@
-import { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import 'quill-image-uploader/dist/quill.imageUploader.min.css';
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "quill-image-uploader/dist/quill.imageUploader.min.css";
+import Form from "react-bootstrap/Form";
+import { getTokenFromCookie } from "../service/TokenService";
+import axios from "axios";
+
+
+
+
 
 
 
 const Write = () => {
-  const [value, setValue] = useState('');
-  console.log(value);
-
-
+  const {token, user} = getTokenFromCookie();
+  console.log(user._id);
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [content, setContent] = useState("");
+  const [imageormedia, setImageormedia] = useState("");
 
   // image preview garne upload garese
   const [imagefile, setImagefile] = useState(null);
+
+  const  handleBlogPostSubmit = async (e) => {
+      e.preventDefault();
+      console.log(title, category, content, imageormedia);
+
+      const postData = {
+        title: title,
+        category: category,
+        content: content,
+        author:user.username,
+        imagesOrMedia: imageormedia,
+        user: user._id
+      }
+
+   
+      axios.post('http://localhost:5000/post/addpost', postData, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          if (error.response) {
+            console.error('Error response data:', error.response.data);
+            console.error('Status code:', error.response.status);
+            console.error('Headers:', error.response.headers);
+          } else if (error.request) {
+            console.error('No response received. Request:', error.request);
+          } else {
+            console.error('Error setting up the request:', error.message);
+          }
+          console.error('Request payload:', postData); // Log the request payload for debugging
+        });
+      
+
+
+  }
+
   function handleChangeImage(e) {
+    // to previe uploaded image
     const file = e.target.files[0];
     if (file) {
       const reader = new FileReader();
@@ -22,80 +73,72 @@ const Write = () => {
       };
       reader.readAsDataURL(file);
     }
+    // to previe uploaded image code ended here
+  }
+
+  const handleEditorChange = (value) => {
+    setContent(value);
   }
 
 
+
   return (
-    <div className='writePost'>
+    <div className="writePost mb-4">
+      <input type="hidden" value="user_id" />
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Blog Title</Form.Label>
+          <Form.Control value={title} onChange={(e)=>setTitle(e.target.value)} type="text" placeholder="Title" />
+        </Form.Group>
+      </Form>
+      <div className="selectCategory mt-4 mb-4 ">
+        <Form.Select value={category} onChange={(e)=>setCategory(e.target.value)} aria-label="Default select example">
+          <option>Select the category</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+      </div>
+
       <div className="editorContainer">
-        <ReactQuill theme="snow" value={value} onChange={setValue} />
-
+        <ReactQuill theme="snow" value={content} onChange={handleEditorChange} />
       </div>
 
+      <div className="imagelinkhere mt-4 mb-4" >
+          <Form.Control type="text" value={imageormedia} onChange={(e)=>setImageormedia(e.target.value)} placeholder="Image Link here "  />
+      </div>
 
-        <div className="menu">
+      <div className="menu">
+        {/* ===================================image preview================================= */}
+        <br />
+        {imagefile && (
+          <img src={imagefile} alt="Image File" style={{ maxWidth: "50%" }} />
+        )}
+        {/* ==================================================================== */}
         <div className="item">
-          {/* <h1>publish</h1>
-            <span>
-              <b>Status: </b> Draft
-            </span>
-            <span>
-              <b>Visibility: </b> Public
-            </span> */}
-
-        <div className="mb-3 mt-4">
-              <label htmlFor="formFile" className="form-label">
-                file input example
-              </label>
-              <input
-                className="form-control"
-                type="file"
-                id="formFile"
-                onChange={handleChangeImage}
-              />
-              <br />
-              {imagefile && <img src={imagefile} alt="Image File" style={{ maxWidth: '50%' }} />}
-            </div>
+          <div className="mb-3 mt-4">
+            <label htmlFor="formFile" className="form-label">
+              Image File  doesn't work currently now
+            </label>
+            <input
+              className="form-control"
+              type="file"
+              id="formFile"
+              onChange={handleChangeImage}
+            />
+            <br />
+          </div>
         </div>
       </div>
-      <div className="item">
-        <h1>category</h1>
-
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="flexRadioDefault"
-            id="art"
-          />
-          <label class="form-check-label" for="art">
-            Art
-          </label>
-        </div>
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="flexRadioDefault"
-            id="science"
-            checked
-          />
-          <label class="form-check-label" for="science">
-            science
-          </label>
-        </div>
-
+      <div className="item mb-4">
         <div className="buttons d-flex gap-2">
-          <button type="button" class="btn btn-primary">
-            Save as Draft
-          </button>
-          <button type="button" class="btn btn-dark">
-            Update
+          <button onClick={handleBlogPostSubmit} type="button mb-4" class="btn btn-dark">
+            Post Blog
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Write
+export default Write;
---
+++ src/service/ToastMessaage.jsx
@@ -0,0 +1,42 @@
+import { toast } from "react-toastify";
+
+
+export const successTOast = (message) => {
+  toast.success(message, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
+};
+export default successTOast;
+
+export const errorToast = (message) => {
+  toast.error(message, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const warningToast = (message) => {
+  toast.warn(message, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
+};
